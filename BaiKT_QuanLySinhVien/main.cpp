d84e7d8af2e2fac0b5d827ac50ae276ea734fde4
GiaKhangCode <tranhuynhgiakhangkg@gmail.com>
2025-03-01 22:14:01 +0700

    Update main.cpp

--- BaiKT_QuanLySinhVien/main.cpp
+++ BaiKT_QuanLySinhVien/main.cpp
@@ -12,8 +12,16 @@ int main()
         List[i].inputInfo();
     for (int i=0; i<n; i++)
         List[i].calculateAverage();
-    cout << "===LIST OF STUDENTS===\n";
+    cout << "\n===LIST OF STUDENTS===\n";
     for (int i=0; i<n; i++)
         List[i].displayInfo();
+    Student* max = &List[0];
+    for (int i=1; i<n; i++)
+    {
+        if (List[i].getAverage() > max->getAverage())
+            max = &List[i];
+    }
+    cout << "\n===STUDENT HAS MAX SCORE===\n";
+    max->displayInfo();
     return 0;
 }
