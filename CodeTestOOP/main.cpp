98b9fe6f2b1a1a61f2b0b33eb869c530f3e22ee7
GiaKhangCode <tranhuynhgiakhangkg@gmail.com>
2025-03-01 22:10:32 +0700

    Update main.cpp

--- CodeTestOOP/main.cpp
+++ CodeTestOOP/main.cpp
@@ -12,8 +12,15 @@ int main()
         List[i].inputInfo();
     for (int i=0; i<n; i++)
         List[i].calculateAverage();
-    cout << "===LIST OF STUDENTS===\n";
+    cout << "\n===LIST OF STUDENTS===\n";
     for (int i=0; i<n; i++)
         List[i].displayInfo();
+    Student* max = &List[0];
+    for (int i=1; i<n; i++)
+    {
+        if (List[i].getAverage() > max->getAverage())
+            max = &List[i];
+    }
+    max->displayInfo();
     return 0;
 }
